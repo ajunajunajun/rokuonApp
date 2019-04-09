dad74457a1692842fd109cce38db004958883e0a
ajunajunajun <ajunnajunn@gmail.com>
2019-04-10 03:19:22 +0900

    WIP

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Audio } from 'expo';
 
 export default class App extends React.Component {
   render() {
+    const recording = new Audio.Recording();
+    async function recStart() {
+      try {
+        await recording.prepareToRecordAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY);
+        await recording.startAsync();
+        // You are now recording!
+      } catch (error) {
+        // An error occurred!
+      }
+    }
+
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Button
+          onPress={this.recStart}
+          title="waao"
+        />
       </View>
     );
   }
