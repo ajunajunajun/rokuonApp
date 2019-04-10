2b4e61ea41f8d85711ec52b2e2bce0cf4f310a95
ajunajunajun <ajunnajunn@gmail.com>
2019-04-11 03:26:11 +0900

    [WIP] maybe add infoPlist

--- App.js
+++ App.js
@@ -3,9 +3,24 @@ import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 import { Audio } from 'expo';
 
 export default class App extends React.Component {
+  
+  async componentDidMount() {
+    try {
+      await Audio.setAudioModeAsync ({
+        allowsRecordingIOS: true,
+        interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+        playsInSilentModeIOS: true,
+        shouldDuckAndroid: true,
+        interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+      });
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   async _recStart() {
     const recording = new Audio.Recording();
+
     try {
       await recording.prepareToRecordAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY);
       await recording.startAsync();
