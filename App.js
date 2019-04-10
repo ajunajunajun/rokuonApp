e009637a3feaad919421a2c2c476d7e47608e42c
ajunajunajun <ajunnajunn@gmail.com>
2019-04-10 11:54:48 +0900

    WIP cant working

--- App.js
+++ App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 import { Audio } from 'expo';
 
 export default class App extends React.Component {
-  render() {
-    const recording = new Audio.Recording();
-    async function recStart() {
-      try {
-        await recording.prepareToRecordAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY);
-        await recording.startAsync();
-        // You are now recording!
-      } catch (error) {
-        // An error occurred!
-      }
-    }
 
+  async _recStart() {
+    const recording = new Audio.Recording();
+    try {
+      await recording.prepareToRecordAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY);
+      await recording.startAsync();
+      Alert.alert('You are now recording!');
+    } catch (error) {
+      Alert.alert('An error occurred!');
+    }
+  }
+  async _recStop() {
+    try {
+      await rocording.stopAndUnloadAsync();
+      Alert.alert('Stop the recording!');
+    } catch (error) {
+      Alert.alert('An error occurred!');
+    }
+  }
+
+  render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Button
-          onPress={this.recStart}
-          title="waao"
+          onPress={this._recStart}
+          title="start"
+        />
+        <Button
+          onPress={this._recStop}
+          title="stop"
         />
       </View>
     );
